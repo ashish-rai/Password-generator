b6c42f24fd0f53bb98cba80308a820b47da878b6
ashish rai <aashishraii182@gmal.com>
2024-12-07 16:04:45 -0700

    Password saving

--- password_generator.py
+++ password_generator.py
@@ -11,12 +11,21 @@ def generate_password(length=10):
 
     return password
 
+def save_password(password):
+
+    #saves the password as txt file
+    file_name = input("Save as: ") .strip() + ".txt"
+    with open(file_name,'w') as file:
+        file.write(password)
+    print(f"Password saved as {file_name}")
+
+
 #Input validation
 while True:
     try:
-
+        print("\n***Welcome to Password Generator!***")
         #Get user input for password length.
-        user_input = input("\n******\nEnter the length of password you want (or PRESS ENTER to AUTO GENERATE):\n******\n")
+        user_input = input("\nEnter the length of password you want (or PRESS ENTER to AUTO GENERATE):")
 
         #If user press Enter (It auto generates the password of default length=10)
         if user_input == "":
@@ -29,7 +38,7 @@ while True:
 
         #Check if the number is positive
         if length <= 0:
-            print("Please enter positive number")
+            print("Number must be greater than 0")
 
         else:
             break #Exits the loop if the valid number is entered
@@ -41,5 +50,14 @@ while True:
 #Generate password after getting valid input
 password = generate_password(length)
 print(f"Password generated: {password}")
+
+#Ask user to save the password or not
+save_pass = input("Do you want to save the password? (y/n):")
+if save_pass == 'y':
+    save_password(password)
+
+else:
+    print("Password not saved")
+
 
 
